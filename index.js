75b09656d3221fa9300de0721021518c6bd933a1
lavinia-firezar <lavinia.firezar@gmail.com>
2022-11-28 13:10:13 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-scrollToTopButton = document.getElementById("scrollToTopButton");
 let currentCity = localStorage.getItem("city");
 
 if (!currentCity) {
@@ -12,11 +11,3 @@ updateCityDisplay(currentCity);
 // Afisam vremea curenta si predictia pe 5 zile.
 displayCurrentWeather(currentCity);
 displayWeatherForecast(currentCity);
-
-scrollToTopButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
